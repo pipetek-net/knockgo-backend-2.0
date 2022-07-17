23215abee29bb19494a701da9513aa1583bfdfd9
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-16 21:53:10 -0400

    Se crean las rutas de los contoladores para enviarlas a app.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+//Se crean las rutas de los contoladores para enviarlas a app.js
 const register = require("./register");
 const login = require("./login");
 const getUserById = require("./getUserById");
