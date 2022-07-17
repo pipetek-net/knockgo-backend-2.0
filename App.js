5e4fb8bdebe96f7e3729095a4f6a7b2b3a301060
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-16 22:05:49 -0400

    Se actualizan parametros de app.js

--- App.js
+++ App.js
@@ -3,21 +3,22 @@ const cors = require('cors');
 const db = require('./database/db');
 
 const controllers = require('./controllers');
+const verifyToken = require("./middlewares/verifyToken");// middleware para verificar el token
 
-const app = express();
+const app = express();// se crea el servidor
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/user/:id', controllers.getUserById);
-app.post('/register', controllers.register);
-app.post('/login', controllers.login);
+app.get("/user", verifyToken, controllers.getUserById); // se crea la ruta para obtener un usuario
+app.post("/register", controllers.register);// se crea la ruta para registrar un usuario
+app.post('/login', controllers.login);// se crea la ruta para el login
 
-const port = 3000;
+const port = 3001; // puerto de express
 
 app.listen(port, () => {
-    console.log(`Backend funcionando en  ${port}`);
-    db();
+    console.log(`Backend funcionando en  ${port}`);// se muestra en consola que el servidor esta funcionando
+    db();// se ejecuta el metodo para conectar a la base de datos
 });
 
 module.exports = app;
