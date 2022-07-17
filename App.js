9791f70e9d4f83a7ed96b214bdfab8b90bb9a9cf
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-16 20:32:40 -0400

    Se crean las rutas para los controladores

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./database/db');
 
+const controllers = require('./controllers');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/user/:id', controllers.getUserById);
+app.post('/register', controllers.register);
+app.post('/login', controllers.login);
 
 const port = 3000;
 
