4d1b0d58d2f3aa19c3952b37e7ae4b4ac0988867
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-16 20:22:15 -0400

    Se agregan variables de conexion en app.js

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./database/db');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const port = 3001;
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Backend funcionando en  ${port}`);
-    }
-);
+    db();
+});
 
 module.exports = app;
